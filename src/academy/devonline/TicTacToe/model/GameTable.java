3e56deb2882d89f203ed1ccbb9c15acb17f02be7
Anton <uis856@gmail.com>
2021-11-01 13:28:38 +0300

    Override to String methods for model clasess

--- src/academy/devonline/TicTacToe/model/GameTable.java
+++ src/academy/devonline/TicTacToe/model/GameTable.java
@@ -1,6 +1,9 @@
 package academy.devonline.TicTacToe.model;
 
+import java.util.Arrays;
+
 public class GameTable {
+
     private final char[][] table = {
             {' ', ' ', ' '},
             {' ', ' ', ' '},
@@ -8,15 +11,28 @@ public class GameTable {
     };
 
     public boolean isEmpty(final Cell cell) {
-        return table[cell.getCol ( )][cell.getRow ( )] == ' ';
-
+        return table[cell.getRow ( )][cell.getCol ( )] == ' ';
     }
 
     public char getSign(final Cell cell) {
-        return table[cell.getCol ( )][cell.getRow ( )];
+        return table[cell.getRow ( )][cell.getCol ( )];
     }
 
     public void setSign(final Cell cell, final char sign) {
-        table[cell.getCol ( )][cell.getRow ( )] = sign;
+        table[cell.getRow ( )][cell.getCol ( )] = sign;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder ("GameTable{");
+        sb.append ("table=");
+        for (int i = 0; i < table.length; i++) {
+            sb.append (Arrays.toString (table[i]));
+            if (i < table.length - 1) {
+                sb.append (';');
+            }
+        }
+        sb.append ('}');
+        return sb.toString ( );
     }
 }
